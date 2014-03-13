a69d07edb842ca15ad5c137027de0a9753c9b470
Eric.Lee <eric-lee-83@hotmail.com>
2014-03-13 15:24:23 +1100

    ..

---
+++ src/com/commons/value/RealEstateValue.java
@@ -0,0 +1,9 @@
+package com.commons.value;
+
+public class RealEstateValue {
+	
+	public static final String REALESTATE_PROPERTIES="com.commons.value.properties.RealEstate";
+	
+	public static final String USER_EMAIL="email";
+	public static final String USER_PASS="pass";
+}
